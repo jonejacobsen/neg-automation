c5efb1ccd8e7208d6c03418008743686355989d5
jonejacobsen <jone.jacobsen@neg-marine.com>
2025-06-29 09:10:30 +0200

    Add Zapier CLI bundle and tests from GPT-4o - index.js

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const { version } = require('./package.json');
+// index.js
+const authentication = require('./authentication');
 
-const helloWorldTrigger = require('./triggers/hello_world');
+const gmailInvitationTrigger = require('./triggers/gmail_invitation');
+const extractGuestInfo = require('./actions/extract_guest_info');
+const createDanishMenu = require('./actions/create_danish_menu');
+const guestAnalysis = require('./actions/guest_analysis');
+const calendarWorkflow = require('./actions/calendar_workflow');
+const prepTimeline = require('./actions/prep_timeline');
 
 module.exports = {
-  version,
+  version: require('./package.json').version,
   platformVersion: require('zapier-platform-core').version,
+  authentication,
   triggers: {
-    [helloWorldTrigger.key]: helloWorldTrigger
+    [gmailInvitationTrigger.key]: gmailInvitationTrigger
+  },
+  searches: {},
+  creates: {
+    [extractGuestInfo.key]: extractGuestInfo,
+    [createDanishMenu.key]: createDanishMenu,
+    [guestAnalysis.key]: guestAnalysis,
+    [calendarWorkflow.key]: calendarWorkflow,
+    [prepTimeline.key]: prepTimeline
   }
 };
-```
-
-```plaintext
-#
