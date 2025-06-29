83de5023015531c5315f66128062031acf9c26b0
jonejacobsen <jone.jacobsen@neg-marine.com>
2025-06-29 09:02:45 +0200

    Add Zapier CLI bundle and tests from GPT-4o - index.js

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const { version } = require('./package.json');
-
-const helloWorldTrigger = require('./triggers/hello_world');
+const authentication = require('./authentication');
+const trigger = require('./triggers/email_invitation');
+const extractGuestInfo = require('./actions/extract_guest_info');
+const createDanishMenu = require('./actions/create_danish_menu');
+const schedulePrepTimeline = require('./actions/schedule_prep_timeline');
 
 module.exports = {
-  version,
+  version: require('./package.json').version,
   platformVersion: require('zapier-platform-core').version,
+  authentication,
   triggers: {
-    [helloWorldTrigger.key]: helloWorldTrigger
+    [trigger.key]: trigger,
+  },
+  actions: {
+    [extractGuestInfo.key]: extractGuestInfo,
+    [createDanishMenu.key]: createDanishMenu,
+    [schedulePrepTimeline.key]: schedulePrepTimeline,
   }
 };
 ```
